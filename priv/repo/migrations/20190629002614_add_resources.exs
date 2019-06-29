a3602e98f0c2273af0e2f1acdfa189924da685be
joeyrosztoczy <joey@wbear.co>
2019-06-28 19:33:28 -0600

    use jsonb

--- priv/repo/migrations/20190629002614_add_resources.exs
+++ priv/repo/migrations/20190629002614_add_resources.exs
@@ -3,7 +3,7 @@ defmodule MinimalUuidCastError.Repo.Migrations.AddResources do
 
   def change do
     create table(:resource_two) do
-      add(:resource_one_id, :map)
+      add(:resource_one_id, :jsonb)
     end
   end
 end
